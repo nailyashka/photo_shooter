18a4759624fad33b7b93f15aa5754dd2cd61e6b3
nailysik <gilazieva@gmal.com>
2021-08-02 16:54:24 +0300

    mail app open functionality

--- lib/notice.dart
+++ lib/notice.dart
@@ -1,10 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:open_mail_app/open_mail_app.dart';
 
 class EmailSentPage extends StatelessWidget {
   const EmailSentPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    void showNoMailAppsDialog(BuildContext context) {
+      showDialog<void>(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text("Open Mail App"),
+            content: Text("No mail apps installed"),
+            actions: <Widget>[
+              FlatButton(
+                child: Text("OK"),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              )
+            ],
+          );
+        },
+      );
+    }
+
+    void openEmail() async {
+      // Android: Will open mail app or show native picker.
+      // iOS: Will open mail app if single mail app found.
+      var result = await OpenMailApp.openMailApp();
+
+      // If no mail apps found, show error
+      if (!result.didOpen && !result.canOpen) {
+        showNoMailAppsDialog(context);
+
+        // iOS: if multiple mail apps found, show dialog to select.
+        // There is no native intent/default app system in iOS so
+        // you have to do it yourself.
+      } else if (!result.didOpen && result.canOpen) {
+        showDialog<void>(
+          context: context,
+          builder: (_) {
+            return MailAppPickerDialog(
+              mailApps: result.options,
+            );
+          },
+        );
+      }
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text('was sent!'),
@@ -26,7 +71,7 @@ class EmailSentPage extends StatelessWidget {
               height: 50,
             ),
             ElevatedButton(
-              onPressed: () {},
+              onPressed: openEmail,
               child: Text('open mail'),
             )
           ],
